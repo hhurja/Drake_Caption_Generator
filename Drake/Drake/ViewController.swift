0f79df01f934be07bcec3ed7300978f07aab0162
Timothy Malaney <timothymalaney@Timothys-MacBook-Pro-2.local>
2018-07-12 10:17:44 -0700

    init project

---
+++ Drake/Drake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drake
+//
+//  Created by Timothy Malaney on 7/12/18.
+//  Copyright © 2018 Boats and Potreros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
